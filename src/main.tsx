eb2aed6330349ca3cfa9c51e00c97b0029efdc53
dragmove <dragmove@naver.com>
2024-06-02 23:29:29 +0900

    🔨 Arrange routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from "./routes/root";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
 import ErrorPage from './error-page';
-import TestSuspense from './routes/test-suspense';
+import Root from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/suspense",
-    element: <TestSuspense />,
     errorElement: <ErrorPage />,
   },
 ]);
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,124 +1,45 @@
-import { Button } from "@/components/ui/button";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-import { useGlobalStore } from "@/store";
-import useSelectDatas from "@/useSelectDatas";
-import { CSSProperties, useEffect, useRef, useState } from "react";
-import { useNavigationType, useSearchParams } from "react-router-dom";
-import { URLSearchParams } from "url";
-import '../index.css';
-import ButtonCtrl from "@/lib/buttonCtrl";
-import Cursor from "@/lib/cursor";
+import AsyncBoundary from "@/components/async-boundary";
+import ContainerA from "@/components/container-a";
+import ContainerB from "@/components/container-b";
+import DataFetchingA from "@/components/data-fetching-a";
+import DataFetchingB from "@/components/data-fetching-b";
+import { ErrorFallback } from "@/components/error-fallback";
+import { Suspense } from "react";
 
-// @see https://nikitahl.com/circle-progress-bar-css
 export default function Root() {
-    const getSearchParamsAll = (params: URLSearchParams) => Object.fromEntries([...params]);
-    
-    const [searchParams, setSearchParams] = useSearchParams();
-    const navigationType = useNavigationType();
+    return <div>
+        {/*
+        <h2>case 1. fetch a data</h2>
+        <Suspense fallback={<Loading />}>
+            <DataFetchingA />
+        </Suspense>
+         */}
 
-    // @see https://tympanus.net/Development/MagneticButtons/index4.html
-    const cursorRef = useRef(null);
-    const btnRef = useRef(null);
-
-    useEffect(() => {
-        const cursor = new Cursor(cursorRef?.current);
-        const button = new ButtonCtrl(btnRef?.current);
-
-        button.on('enter', () => cursor.enter());
-        button.on('leave', () => cursor.leave());
-    }, [])
-    
-    return <>
-        <svg className="cursor" width="25" height="25" viewBox="0 0 25 25" ref={cursorRef}>
-			<circle className="cursor__inner" cx="12.5" cy="12.5" r="6.25"/>
-		</svg>
-        
-        <div className="content">
-            <button className="button" ref={btnRef}>
-                <div className="button__filler"></div>
-                <span className="button__text">
-                    <span className="button__text-inner">Watch</span>
-                </span>
-            </button>
-        </div>
+        {/* 
+        <h2>case 2. fetch multiple datas</h2>
+        <Suspense fallback={<Loading />}>
+            <DataFetchingA />
+            <DataFetchingB />
+        </Suspense>
+         */}
 
         
-    </>
-    
+        {/* 
+        <h2>case 3. fetch multiple datas in containers</h2>
+        <Suspense fallback={<Loading />}>
+            <ContainerA />
+            <ContainerB />
+        </Suspense>
+         */}
 
-    /*
-    // progress loading
-    const progressStyle: CSSProperties = {
-        visibility: 'hidden',
-        height: 0,
-        width: 0
-    }
-
-    // return <div className="progress-bar" role="progressbar" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
-    return <div className="progress-bar">
-        <progress value={75} max={100} style={progressStyle}>75%</progress>
+        <h2>case 4. apply AsyncBoundary</h2>
+        <AsyncBoundary ErrorFallback={ErrorFallback} SuspenseFallback={<Loading />}>
+            <DataFetchingA />
+            <DataFetchingB />
+        </AsyncBoundary>
     </div>
-    */
+}
 
-    /*
-    // headless component
-    const world = useGlobalStore((state) => state.world);
-    const setWorld = useGlobalStore((state) => state.setWorld);
-
-    const [season, setSeason] = useState();
-
-    const { foo } = useSelectDatas({ world, season, navigationType });
-
-    const handleChangeWorld = (val) => {
-        setWorld(val);
-
-        // const season = searchParams.get('season') ?? ''
-        setSearchParams({
-            ...getSearchParamsAll(searchParams),
-            world: val,
-        });
-    };
-    const handleChangeSeason = (val) => {
-        setSeason(val);
-
-        // const world = searchParams.get('world') ?? ''
-        setSearchParams({
-            ...getSearchParamsAll(searchParams),
-            season: val
-        });
-    };
-    
-    return (
-        <div>
-            <Button>Click Me</Button>
-
-            <Select onValueChange={handleChangeWorld}>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="select world" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="1">사신수</SelectItem>
-                    <SelectItem value="2">글로벌</SelectItem>
-                    <SelectItem value="3">아랑</SelectItem>
-                </SelectContent>
-            </Select>
-
-            <Select onValueChange={handleChangeSeason}>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="select season" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="2">시즌 2</SelectItem>
-                    <SelectItem value="1">시즌 1</SelectItem>
-                </SelectContent>
-            </Select>
-        </div>
-    );
-    */
+function Loading() {
+    return <div>Loading...</div>;
 }
